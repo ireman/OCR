1c906ea4824cc053e464dd7f008e59065d31d80c
ireman <ireman.br@gmail.com>
2021-10-26 22:09:06 +0300

    1. adding unicode_to_label_dict as instance attribute of AnnotationAndPrediction class. 2. adding label_to_unicode_dict as instance attribute of AnnotationAndPrediction class. 3. Change width and height to the actual width and height. 4. changing the predict step from one predict at the time to all predictions at once.

--- AnnotationAndPrediction.py
+++ AnnotationAndPrediction.py
@@ -15,7 +15,7 @@ def union(box1,box2):
 
 
 def intersection(box1,box2):
-    if 2 < box1[2]-box2[0] < 50 and abs(box1[3]-box2[3])<20:
+    if 2 < box1[2]-box2[0] < 50 and abs(box1[3]-box2[3]) < 20:
         return True
     return False
 
@@ -43,9 +43,22 @@ class AnnotationAndPrediction:
     2. Unicode prediction appropriate for each sign.
     """
 
-    def __init__(self, model_path='cyrus_classifier_filtered_test_not_aug.pt', labels_csv='filtered_label.csv'):
+    def __init__(self,
+                 model_path='cyrus_classifier_filtered_test_not_aug.pt',
+                 labels_csv='filtered_label.csv',
+                 unicode_to_label_dict='unicode_to_label_dict.csv',
+                 label_to_unicode_dict='label_to_unicode_dict.csv'):
+
         df = pd.read_csv(labels_csv, keep_default_na=False, encoding='utf-8')
         self.label_list = df['label'].tolist()
+
+        df = pd.read_csv(unicode_to_label_dict, keep_default_na=False, encoding='utf-8')
+        self.unicode_to_label_dict = df.set_index('label').T.to_dict('list')
+        self.unicode_to_label_dict = {k: list(filter(None, v)) for k, v in self.unicode_to_label_dict.items()}
+
+        df = pd.read_csv(label_to_unicode_dict, keep_default_na=False, encoding='utf-8', header=None)
+        self.label_to_unicode_dict = df.set_index(0).astype(str).agg(' '.join, axis=1).to_dict()
+
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model = torchvision.models.resnet18(pretrained=True)
         num_ftrs = model.fc.in_features
@@ -92,10 +105,10 @@ class AnnotationAndPrediction:
                 crop_img = gray[Y - 1:Y + H + 1, X - 1:X + W + 1]
                 zero_pixel = (np.sum(crop_img == 0) / crop_img.size) * 100
                 if zero_pixel >= 12:
-                    x_vals.append(X - 1)
-                    y_vals.append(Y - 1)
-                    w_vals.append(X + W + 1)
-                    h_vals.append(Y + H + 1)
+                    x_vals.append(X)
+                    y_vals.append(Y)
+                    w_vals.append(X + W)
+                    h_vals.append(Y + H)
 
         data = {'X': x_vals, 'Y': y_vals, 'W': w_vals, 'H': h_vals}
         df = pd.DataFrame(data)
@@ -120,8 +133,8 @@ class AnnotationAndPrediction:
         df1 = pd.DataFrame()
         df1['X'] = fixed_boxes[:,0]
         df1['Y'] = fixed_boxes[:,1]
-        df1['W'] = fixed_boxes[:,2]
-        df1['H'] = fixed_boxes[:,3]
+        df1['W'] = fixed_boxes[:,2] - fixed_boxes[:,0]
+        df1['H'] = fixed_boxes[:,3] - fixed_boxes[:,1]
 
         bbox = df1.values[:, 0:4]
         y_val = bbox[0][1]
@@ -129,7 +142,7 @@ class AnnotationAndPrediction:
         line_num = 1
         for k, box in enumerate(bbox):
             y_curr = box[1]
-            if abs(y_val - y_curr) <= 25:
+            if abs(y_val - y_curr) <= 22:
                 line_index.append(line_num)
             else:
                 y_val = y_curr
@@ -150,27 +163,24 @@ class AnnotationAndPrediction:
         txt = ''
         line_num = 1
         m = 0
+        crop_img_list = []
         for box in bbox:
             X, Y, W, H = box[0:4]
             curr_line = box[4]
-            crop_img1 = image[Y:H, X:W]
+            crop_img1 = image[Y:Y + H, X:X + W]
             crop_img = cv2.resize(crop_img1, dsize=(64, 64))
             crop_img = (np.asarray(crop_img).reshape(64, 64, 3).astype(np.float64)) / 255
             trans = transforms.ToTensor()
             crop_img = trans(crop_img)
             crop_img = crop_img.to(device=self.device)
-            self.model.eval()
-            with torch.no_grad():
-                scores = self.model(crop_img[None, ...])
-                _, predictions = scores.max(1)
+            crop_img_list.append(crop_img)
+        torch_images = torch.stack(crop_img_list)
+        self.model.eval()
+        with torch.no_grad():
+            scores = self.model(torch_images)
+            for score in scores:
+                _, predictions = score.max(0)
                 predictions_list.append(self.label_list[predictions])
-            if line_num == curr_line:
-                txt += self.label_list[predictions]+' '
-            else:
-                txt += '\n' + self.label_list[predictions]+' '
-                line_num += 1
-
         df['predictions'] = predictions_list
 
         return df
-
---
+++ test_AnnotationAndPrediction.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_union(self):
+        pass
+
+    def test_intersection(self):
+        pass
+
+    def test_combine_boxes(self):
+        pass
+
+    def test_annotation_and_prediction(self):
+        pass
